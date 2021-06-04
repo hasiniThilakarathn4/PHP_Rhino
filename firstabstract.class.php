461b590df126d174ea023b0db2e29243a7034a86
hasini_thilakarathna <49187599+hasiniThilakarathn4@users.noreply.github.com>
2021-06-04 19:11:30 +0530

    Add files via upload

--- firstabstract.class.php
+++ firstabstract.class.php
@@ -1,6 +1,6 @@
 <?php
 abstract class AbstractClass {
-    //contains at least one abstract method-->different from interfaces
+    //contains at least one abstract method
   abstract public function method1();
   abstract public function method2($name);
   
